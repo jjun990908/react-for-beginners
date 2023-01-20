7d9ff82580cf86f98f8a0635548d61eab753428f
jjun990908 <qkrwnsdud@kookmin.ac.kr>
2023-01-20 20:55:48 +0900

    링크 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App";
-import CoinTracker from "./CoinTracker";
+// import CoinTracker from "./CoinTracker";
+import MovieSite from "./MovieSite";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<CoinTracker />);
+// root.render(<App />);
+// root.render(<CoinTracker />);
+root.render(<MovieSite />);
